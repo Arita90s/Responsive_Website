c597ba76b93f10ab3236da33ca3e23dd160d8ad4
sjs <aritawebm10@gmail.com>
2026-02-02 10:48:02 +0900

    FAQページ作成

--- js/script.js
+++ js/script.js
@@ -10,3 +10,38 @@ globalNavBtn.addEventListener('click', () => {
 globalNavCloseBtn.addEventListener('click', () => {
     document.body.classList.remove('is-open');
 });
+
+
+// FAQ
+const btns = document.querySelectorAll('.p-faq__listBtn');
+const answers = document.querySelectorAll('.p-faq__listA');
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const currentBtn = e.target;
+        const parent = currentBtn.closest('.p-faq__listItem');
+        const currentAnswer = parent.querySelector('.p-faq__listA');
+        console.log(currentAnswer);
+
+        const isCurrentOpen = currentBtn.classList.contains('is-open');
+        console.log(isCurrentOpen);
+
+        // 全ボタン・アンサーからis-openクラスを削除
+        btns.forEach((btn) => {
+            btn.classList.remove('is-open');
+        });
+        answers.forEach((answer) => {
+            answer.classList.remove('is-open');
+        });
+
+        if (isCurrentOpen) {
+            // 開いた状態スタート
+            currentBtn.classList.remove('is-open');
+            currentAnswer.classList.remove('is-open');
+        } else {
+            // 閉じた状態スタート
+            currentBtn.classList.add('is-open');
+            currentAnswer.classList.add('is-open');
+        }
+    });
+});
